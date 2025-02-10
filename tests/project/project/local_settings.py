cca2c27981ba8e1ebc77708082dd22fa03bf9bfe
Geoffrey Eisenbarth <geoffrey.eisenbarth@gmail.com>
2025-02-09 23:04:44 -0600

    Remove settings

--- tests/project/project/local_settings.py
+++
@@ -1,5 +0,0 @@
-CTCT_PUBLIC_KEY = 'ff576a6d-5679-4c7d-85f3-777fe96f02a2'
-CTCT_SECRET_KEY = '7WpdxoLNadDclJ2_dR20yw'
-CTCT_REDIRECT_URI = 'http://www.ironbeard.org:8000/accounts/ctct/auth/'
-CTCT_FROM_NAME = "Django CTCT"
-CTCT_FROM_EMAIL = "django@ctct.com"
