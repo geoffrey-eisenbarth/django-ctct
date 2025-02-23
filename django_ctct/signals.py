682bd303439c3f403e5c7a1e1eab6e6f7dfa6bd5
Geoffrey Eisenbarth <geoffrey.eisenbarth@gmail.com>
2025-02-22 22:48:27 -0600

    Simplify

--- django_ctct/signals.py
+++ django_ctct/signals.py
@@ -1,11 +1,14 @@
 from typing import Type, Optional
 
+from django.conf import settings
 from django.db.models import Model
 
-from django_ctct.models import CTCTModel, CTCTLocalModel, Contact, ContactList
+from django_ctct.models import (
+  CTCTModel, Contact, ContactList, ContactAndContactList,
+)
 
 
-def ctct_save(
+def remote_save(
   sender: Type[Model],
   instance: Model,
   created: bool,
@@ -13,79 +16,52 @@ def ctct_save(
   **kwargs,
 ) -> None:
   """Create or update the instance on CTCT servers."""
-  if isinstance(instance, CTCTLocalModel):
-    return
-  elif isinstance(instance, CTCTModel) and instance.save_to_ctct:
+
+  if isinstance(instance, CTCTModel) and instance.save_to_ctct:
     sender.remote.connect()
     if instance.api_id:
-      method = sender.remote.update
+      task = sender.remote.update
     else:
-      method = sender.remote.create
+      task = sender.remote.create
 
-    if instance.save_to_ctct == 'sync':
-      method(instance)
-    elif instance.save_to_ctct == 'async':
-      method(instance).enqueue()
+    if getattr(instance, 'enqueue', settings.CTCT_ENQUEUE_DEFAULT):
+      task.enqueue(obj=instance)
+    else:
+      task(obj=instance)
 
 
-def ctct_delete(sender, instance, **kwargs) -> None:
+def remote_delete(sender, instance, **kwargs) -> None:
   """Delete the instance from CTCT servers."""
-  if isinstance(instance, CTCTLocalModel):
-    return
-  elif isinstance(instance, CTCTModel) and instance.save_to_ctct:
+
+  if isinstance(instance, CTCTModel) and instance.save_to_ctct:
     sender.remote.connect()
-    method = sender.remote.delete
+    task = sender.remote.delete
 
-    if instance.save_to_ctct == 'sync':
-      method(instance)
-    elif instance.save_to_ctct == 'async':
-      method(instance).enqueue()
+    if getattr(instance, 'enqueue', settings.CTCT_ENQUEUE_DEFAULT):
+      task.enqueue(obj=instance)
+    else:
+      task(obj=instance)
 
 
-def ctct_update_contact_lists(sender, instance, action, **kwargs):
+def remote_update_m2m(sender, instance, action, **kwargs):
   """Updates a Contact's list membership on CTCT servers."""
 
-  if action in ['post_add', 'post_remove', 'post_clear']:
+  actions = ['post_add', 'post_remove', 'post_clear']
+  if (sender is ContactAndContactList) and (action in actions):
 
     if isinstance(instance, Contact):
-      if instance.save_to_ctct == 'sync':
-        ctct_update_lists_task(instance)
-      elif instance.save_to_ctct == 'async':
-        ctct_update_lists_task.delay(instance)
-
+      Contact.remote.connect()
+      task = Contact.remote.update
+      kwargs = {'obj': instance}
     elif isinstance(instance, ContactList):
-      contacts = Contact.objects.filter(pk__in=kwargs['pk_set'])
-      if instance.save_to_ctct == 'async':
-        ctct_add_list_memberships_task(instance, contacts)
-      elif instance.save_to_ctct == 'async':
-        ctct_add_list_memberships_task.delay(instance, contacts)
+      ContactList.remote.connect()
+      task = ContactList.remote.add_list_memberships
+      kwargs = {
+        'contact_list': instance,
+        'contacts': Contact.objects.filter(pk__in=kwargs['pk_set']),
+      }
 
-
-# TODO: Refactor these tasks
-# def ctct_update_lists_task(contact: 'Contact') -> None:
-#   """Update ContactLists on CTCT, or delete Contact if no ContactLists.
-# 
-#   Notes
-#   -----
-#   Due to the way Django admin saves related models, I haven't been able
-#   to determine a good way to address this other than just delaying this
-#   method for a few minutes.
-# 
-#   The primary issue is that we want to make sure that a Contact.ctct_save()
-#   call isn't made after this call, since that will revive the Contact in
-#   the event that they had been deleted from CTCT servers due to no longer
-#   belonging to any ContactLists (CTCT requires that Contacts must belong to
-#   at least one ContactList).
-# 
-#   """
-# 
-#   if contact.api_id is not None:
-#     sleep(60 * 1)  # 1 minute
-#     contact.ctct_update_lists()
-# 
-# 
-# def ctct_add_list_memberships_task(
-#   contact_list: 'ContactList',
-#   contacts: QuerySet['Contact'],
-# ) -> None:
-#   contact_list.ctct_add_list_memberships(contacts)
+    if getattr(instance, 'enqueue', settings.CTCT_ENQUEUE_DEFAULT):
+      task.enqueue(**kwargs)
+    else:
+      task(**kwargs)
