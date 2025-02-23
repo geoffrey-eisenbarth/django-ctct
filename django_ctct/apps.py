75b3a354ce11d671ff298932df10ce49e9e57571
Geoffrey Eisenbarth <geoffrey.eisenbarth@gmail.com>
2025-02-22 22:47:24 -0600

    Move setting validation and clean up signals

--- django_ctct/apps.py
+++ django_ctct/apps.py
@@ -1,22 +1,36 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_save, m2m_changed, pre_delete
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 
 class CTCTConfig(AppConfig):
   name = 'django_ctct'
   verbose_name = 'Constant Contact'
+  ctct_settings = [
+    'CTCT_PUBLIC_KEY',
+    'CTCT_SECRET_KEY',
+    'CTCT_REDIRECT_URI',
+    'CTCT_FROM_NAME',
+    'CTCT_FROM_EMAIL',
+    'CTCT_ENQUEUE_DEFAULT',
+  ]
 
   def ready(self):
     from django_ctct.signals import (
-      ctct_save, ctct_delete, ctct_update_contact_lists,
+      remote_save, remote_delete, remote_update_m2m
     )
 
+    # Validate that necessary settings have been defined
+    for value in self.ctct_settings:
+      if not hasattr(settings, value):
+        message = (
+          f"[django-ctct] {value} must be defined in settings.py."
+        )
+        raise ImproperlyConfigured(message)
 
+    # Hook up the signals
     if getattr(settings, 'CTCT_USE_SIGNALS', False):
-      post_save.connect(ctct_save)
-      pre_delete.connect(ctct_delete)
-      m2m_changed.connect(
-        ctct_update_contact_lists,
-        sender='django_ctct.Contact.list_memberships.through',
-      )
+      post_save.connect(remote_save)
+      pre_delete.connect(remote_delete)
+      m2m_changed.connect(remote_update_m2m)
